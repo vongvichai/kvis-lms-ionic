c14056e72ae8fc2e9081105cff5b392e18af5ad0
Vichai Vongvorakul <vongvichai@gmail.com>
2019-05-12 10:17:44 +0700

    initialize main screen

---
+++ web/src/templates/menu.js
@@ -0,0 +1,18 @@
+exports.render = path => {
+    return `
+        <ion-app>
+            <ion-split-pane>
+                <!--  our side menu  -->
+                <ion-menu>
+                    <ion-header>
+                        <ion-toolbar>
+                            <ion-title>Menu</ion-title>
+                        </ion-toolbar>
+                    </ion-header>
+                </ion-menu>
+            
+                <!-- the main content -->
+                <ion-router-outlet main></ion-router-outlet>
+            </ion-split-pane>
+        </ion-app>`
+};
