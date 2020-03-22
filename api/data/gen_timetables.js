f1f396bf10a4331058981f20a9c7ea1d12097b91
Vichai Vongvorakul <vongvichai@gmail.com>
2020-03-22 22:31:29 +0700

    Add fromDate/toDate to table schedules

--- api/data/gen_timetables.js
+++ api/data/gen_timetables.js
@@ -14,72 +14,54 @@ TimeTables.belongsToMany(Stations, { through: 'timeTable_station' });
 Schedules.hasMany(TimeTables);
 
 const schedules_array = [{
-        "expired": "2019-06-26 11:24:45",
+        "fromDate": "2019-05-01 00:00:00",
+        "toDate": "2019-05-04 00:00:00",
         "timeTable": [{ "trip": 1, "out_kvis": "", "in_kvis": "2019-05-06 16:00:00" }]
     },
     {
-        "expired": "2019-06-26 11:24:45",
+        "fromDate": "2019-05-07 00:00:00",
+        "toDate": "2019-05-09 00:00:00",
         "timeTable": [{ "trip": 2, "out_kvis": "2019-05-10 17:00:00", "in_kvis": "2019-05-11 16:00:00" },
             { "trip": 2, "out_kvis": "", "in_kvis": "2019-05-12 07:00:00" }
         ]
     },
     {
-        "expired": "2019-06-26 11:24:45",
+        "fromDate": "2019-05-12 00:00:00",
+        "toDate": "2019-05-14 00:00:00",
         "timeTable": [{ "trip": 3, "out_kvis": "2019-05-17 17:00:00", "in_kvis": "2019-05-20 16:00:00" },
             { "trip": 3, "out_kvis": "2019-05-18 08:30:00", "in_kvis": "" }
         ]
     },
     {
-        "expired": "2019-06-26 11:24:45",
+        "fromDate": "2019-05-19 00:00:00",
+        "toDate": "2019-05-21 00:00:00",
         "timeTable": [{ "trip": 4, "out_kvis": "2019-05-24 17:00:00", "in_kvis": "2019-05-26 16:00:00" },
             { "trip": 4, "out_kvis": "2019-05-25 13:00:00", "in_kvis": "" }
         ]
     },
     {
-        "expired": "2019-06-26 11:24:45",
+        "fromDate": "2019-05-27 00:00:00",
+        "toDate": "2019-05-30 00:00:00",
         "timeTable": [{ "trip": 5, "out_kvis": "2019-05-31 17:00:00", "in_kvis": "2019-06-03 16:00:00" },
             { "trip": 5, "out_kvis": "2019-06-01 08:30:00", "in_kvis": "" },
             { "trip": 5, "out_kvis": "2019-06-01 13:00:00", "in_kvis": "" }
         ]
     },
-    { "expired": "2019-06-26 11:24:45", "timeTable": [{ "trip": 6, "out_kvis": "2019-06-07 17:00:00", "in_kvis": "2019-06-09 16:00:00" }] },
-    { "expired": "2019-06-26 11:24:45", "timeTable": [{ "trip": 7, "out_kvis": "2019-06-14 17:00:00", "in_kvis": "2019-06-16 16:00:00" }] },
-    { "expired": "2019-06-26 11:24:45", "timeTable": [{ "trip": 8, "out_kvis": "2019-06-21 17:00:00", "in_kvis": "2019-06-23 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 9, "out_kvis": "2019-06-28 17:00:00", "in_kvis": "2019-06-30 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 10, "out_kvis": "2019-07-05 17:00:00", "in_kvis": "2019-07-07 16:00:00" }] },
     {
-        "expired": null,
-        "timeTable": [{ "trip": 11, "out_kvis": "2019-07-12 17:00:00", "in_kvis": "2019-07-21 16:00:00" },
-            { "trip": 11, "out_kvis": "2019-07-13 08:30:00", "in_kvis": "" }
-        ]
+        "fromDate": "2019-06-02 00:00:00",
+        "toDate": "2019-06-04 00:00:00",
+        "timeTable": [{ "trip": 6, "out_kvis": "2019-06-07 17:00:00", "in_kvis": "2019-06-09 16:00:00" }]
     },
-    { "expired": null, "timeTable": [{ "trip": 12, "out_kvis": "2019-07-26 17:00:00", "in_kvis": "2019-07-29 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 13, "out_kvis": "2019-07-27 08:30:00", "in_kvis": "" }] },
-    { "expired": null, "timeTable": [{ "trip": 14, "out_kvis": "2019-08-02 17:00:00", "in_kvis": "2019-08-04 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 15, "out_kvis": "2019-08-09 17:00:00", "in_kvis": "2019-08-12 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 15, "out_kvis": "2019-08-10 14:00:00", "in_kvis": "" }] },
-    { "expired": null, "timeTable": [{ "trip": 16, "out_kvis": "2019-08-16 17:00:00", "in_kvis": "2019-08-18 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 17, "out_kvis": "2019-08-23 17:00:00", "in_kvis": "2019-08-25 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 18, "out_kvis": "2019-08-30 17:00:00", "in_kvis": "2019-09-01 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 19, "out_kvis": "2019-09-06 17:00:00", "in_kvis": "2019-09-08 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 20, "out_kvis": "2019-09-13 17:00:00", "in_kvis": "2019-09-15 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 21, "out_kvis": "2019-09-20 17:00:00", "in_kvis": "2019-09-22 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 22, "out_kvis": "2019-09-27 17:00:00", "in_kvis": "2019-10-14 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 22, "out_kvis": "2019-09-28 08:30:00", "in_kvis": "" }] },
-    { "expired": null, "timeTable": [{ "trip": 23, "out_kvis": "2019-10-18 17:00:00", "in_kvis": "2019-10-20 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 24, "out_kvis": "2019-10-25 17:00:00", "in_kvis": "2019-10-27 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 25, "out_kvis": "2019-11-01 17:00:00", "in_kvis": "2019-11-03 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 26, "out_kvis": "2019-11-15 17:00:00", "in_kvis": "2019-11-17 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 27, "out_kvis": "2019-11-22 17:00:00", "in_kvis": "2019-11-24 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 28, "out_kvis": "2019-11-29 17:00:00", "in_kvis": "2019-12-01 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 29, "out_kvis": "2019-12-06 17:00:00", "in_kvis": "2019-12-08 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 30, "out_kvis": "2019-12-13 17:00:00", "in_kvis": "2019-12-15 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 31, "out_kvis": "2019-12-20 17:00:00", "in_kvis": "2019-12-22 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 32, "out_kvis": "2019-12-27 17:00:00", "in_kvis": "2020-01-05 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 32, "out_kvis": "2019-12-28 08:30:00", "in_kvis": "" }] },
-    { "expired": null, "timeTable": [{ "trip": 34, "out_kvis": "2020-01-10 17:00:00", "in_kvis": "2020-01-12 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 35, "out_kvis": "2020-01-24 17:00:00", "in_kvis": "2020-01-26 16:00:00" }] },
-    { "expired": null, "timeTable": [{ "trip": 36, "out_kvis": "2020-01-31 17:00:00", "in_kvis": "2020-02-02 16:00:00" }] }
+    {
+        "fromDate": "2019-06-10 00:00:00",
+        "toDate": "2019-06-12 00:00:00",
+        "timeTable": [{ "trip": 7, "out_kvis": "2019-06-14 17:00:00", "in_kvis": "2019-06-16 16:00:00" }]
+    },
+    {
+        "fromDate": "2019-06-17 00:00:00",
+        "toDate": "2019-06-20 00:00:00",
+        "timeTable": [{ "trip": 8, "out_kvis": "2019-06-21 17:00:00", "in_kvis": "2019-06-23 16:00:00" }]
+    },
 ];
 
 Schedules.sync({ force: true }).done(async() => {
@@ -89,7 +71,10 @@ Schedules.sync({ force: true }).done(async() => {
     schedules_array.map(o => {
         // let { expired, timeTable: [trip, ...restTrip] } = o;
         // console.log(expired, trip, restTrip);
-        Schedules.create({ expired: new Date(o.expired + 'Z') })
+        Schedules.create({
+                fromDate: new Date(o.fromDate + 'Z'),
+                toDate: new Date(o.toDate + 'Z')
+            })
             .then(s => {
                 o.timeTable.map(t => {
                     if (t.out_kvis) {
@@ -118,3 +103,25 @@ Schedules.sync({ force: true }).done(async() => {
     });
 
 });
+
+TimeTables.findAll({
+        where: {
+            tripType: 'OUT',
+            trip: 22
+        },
+        attributes: ['id',
+            'trip',
+            'tripType',
+            'datetime'
+        ],
+        include: [{
+            model: Stations,
+            attributes: ['id', 'name'],
+            required: false,
+        }],
+        nest: true,
+        raw: true
+    })
+    .then(ts => {
+        console.log(ts);
+    })
