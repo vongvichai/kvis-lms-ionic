91260cf39bb59066442aba7cfa2768dc97f276e1
Vichai Vongvorakul <vongvichai@gmail.com>
2020-03-22 21:12:25 +0700

    Add 'fromDate' and 'toDate', Modified expired to VIRTUAL

--- api/models/schedules.js
+++ api/models/schedules.js
@@ -6,9 +6,17 @@ module.exports = (sequelize, DataType) => {
             primaryKey: true,
             autoIncrement: true
         },
+        fromDate: {
+            type: DataType.DATE
+        },
+        toDate: {
+            type: DataType.DATE
+        },
         expired: {
-            type: DataType.DATE,
-            allowNull: true
+            type: DataType.VIRTUAL,
+            get() {
+                return Date.now() > this.getDataValue('toDate');
+            }
         }
     }, {
         timestamps: true,
